1a9028ea7dc02a6f1296c032d00de3f33a8ba8f2
cjfeder <cjfeder17@gmail.com>
2017-05-16 20:30:04 -0700

    Create read.R

---
+++ read.R
@@ -0,0 +1,16 @@
+setwd("C:/Users/conra/Desktop")
+file <- read.csv("train.csv", header = TRUE, sep = ",")
+prop = .1
+indexs <- sample(1:nrow(file),size = (prop)*(nrow(file)), 
+                 replace = FALSE)
+df <- file[indexs, ]
+print(df)
+colnames(df) <- c("Dates","Category","Description","Day of Week", "Police District", "Resolution","Address","X","Y")
+
+library(leaflet)
+sf_map <- leaflet() %>%
+  addTiles()
+sf_map = leaflet()
+sf_map = addTiles(sf_map)
+sf_map <- sf_map %>%
+sf_map
