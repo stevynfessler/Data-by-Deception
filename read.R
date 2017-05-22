e1a826539d1aafbbf462799c7edb08c3f05cc271
JayDSingh <jay@umail.ucsb.edu>
2017-05-22 16:27:55 -0700

    Got the points on the map in Shiny

--- read.R
+++ read.R
@@ -1,14 +1,7 @@
 setwd("/Users/Jay/Google Drive/Rstudio_WD")
 file <- read.csv("train.csv", header = TRUE, sep = ",")
-prop = .1
+prop = .01
 indexs <- sample(1:nrow(file),size = (prop)*(nrow(file)), replace = FALSE)
 df <- file[indexs, ]
 #print(df)
 colnames(df) <- c("Dates","Category","Description","Day of Week", "Police District", "Resolution","Address","X","Y")
-
-library(leaflet)
-sf_map <- leaflet() %>%
-    addTiles()
-sf_map = leaflet()
-sf_map = addTiles(sf_map)
-sf_map
--- server.R
+++ server.R
@@ -4,17 +4,11 @@ library(leaflet)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  
   output$map <- renderLeaflet({
-    leaflet() %>% 
-    addTiles() %>% 
-    addMarkers(lng = -119.8489, lat = 34.4140, popup = "University of Casual Sex and Beer")
+    m = leaflet(df) %>% 
+    addTiles()
+    m %>% setView(lng = -122.4194, lat = 37.7749, zoom = 10)
+    m %>% addCircles(~X, ~Y, popup = df$Description)
 })
     }
       )
