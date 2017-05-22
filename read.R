1c3bcf93ce7d4d52af0e8298499428b7efc8dfbd
JayDSingh <jay@umail.ucsb.edu>
2017-05-22 13:52:47 -0700

    modified read.R a bit

--- read.R
+++ read.R
@@ -1,16 +1,14 @@
-setwd("C:/Users/conra/Desktop")
+setwd("/Users/Jay/Google Drive/Rstudio_WD")
 file <- read.csv("train.csv", header = TRUE, sep = ",")
 prop = .1
-indexs <- sample(1:nrow(file),size = (prop)*(nrow(file)), 
-                 replace = FALSE)
+indexs <- sample(1:nrow(file),size = (prop)*(nrow(file)), replace = FALSE)
 df <- file[indexs, ]
-print(df)
+#print(df)
 colnames(df) <- c("Dates","Category","Description","Day of Week", "Police District", "Resolution","Address","X","Y")
 
 library(leaflet)
 sf_map <- leaflet() %>%
-  addTiles()
+    addTiles()
 sf_map = leaflet()
 sf_map = addTiles(sf_map)
-sf_map <- sf_map %>%
 sf_map
